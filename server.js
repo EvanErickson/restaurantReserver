ef6953a03d0ffd51a821d63d3e1f97b8c2af3bee
EvanErickson <evanserickson@gmail.com>
2020-02-19 13:14:28 -0800

    added index

--- server.js
+++ server.js
@@ -4,4 +4,49 @@ const path = require('path')
 const axios = require('axios')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000)
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/employees', (req, res) => {
+//   fs.readFile('employees.json', 'utf8', (e, data) => {
+//     if (e) { console.log(e) }
+
+//     const employees = JSON.parse(data)
+
+//     res.json(employees)
+//   })
+// })
+
+// app.post('/employees', (req, res) => {
+//   fs.readFile('employees.json', 'utf8', (e, data) => {
+//     if (e) { console.log(e) }
+
+//     const employees = JSON.parse(data)
+
+//     employees.push(req.body)
+
+//     fs.writeFile('employees.json', JSON.stringify(employees), e => {
+//       if (e) { console.log(e) }
+//       res.sendStatus(200)
+//     })
+//   })
+// })
+
 
